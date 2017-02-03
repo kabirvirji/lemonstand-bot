7ad982e8a7fb12cb7b30e881b3c0174a7a7f89fb
Kabir Virji <kabirvirji@gmail.com>
2017-02-03 00:37:14 -0500

    quick replies

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', function (req, res) {
       let event = req.body.entry[0].messaging[i]
       let sender = event.sender.id
       if (event.message){
-        quickReplies(sender)
+        //quickReplies(sender)
       }
 
       if (event.message && event.message.text) {
