d0e3610e7d23d2543e1caba93390f1efd74dfda1
Kabir Virji <kabirvirji@gmail.com>
2017-02-06 18:25:19 -0500

    get started

--- index.js
+++ index.js
@@ -77,8 +77,12 @@ app.get('/', function (req, res) {
                 sendTextMessage(sender, "We reserve the best for the best. Starting at $399/month we provide the best service an e-commerce platform has to offer. This includes everything the Growth plan does, plus dedicated support and higher API limits. Definitely a go to for any medium to large sized business.")
             }
             if (event.postback.payload === 'Get Started') {
+                var c = 0;
                 sendTextMessage(sender, "Hi, welcome to LemonStand 🍋 What can I help you with today?")
+                c++;
+                if (c === 1) {
                 quickReplies(sender)
+                }   
             }
             if (event.postback.payload === 'Help') {
                 sendTextMessage(sender, "Please email support@lemonstand.com with any questions.")
