ab337313997773dc996309f863fa991f844a5fe7
Kabir Virji <kabirvirji@gmail.com>
2017-02-06 12:37:25 -0500

    first of the day

--- index.js
+++ index.js
@@ -66,11 +66,10 @@ app.get('/', function (req, res) {
     if (event.postback && event.postback.payload) {
         //let text1 = JSON.stringify(event.postback)
         //var obj1 = JSON.parse(text)
-        console.log(event.postback.payload)
-        //if (obj1.payload === "Professional") {
-
+        //console.log(event.postback.payload)
+            if (event.postback.payload === 'Professional') {
             sendTextMessage(sender, "The Professional plan is meant for serious small businesses. It starts at $99/month and includes all the basic LemonStand features from cross-platform response themes, to sales analytics and much more!")
-         //}   
+            }   
         }
       if (event.postback) {
         let text = JSON.stringify(event.postback)
